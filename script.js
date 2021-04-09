2ddc86cebb95e61128c084336c02eb5ec52668d2
weversonneri <weversonneri@outlook.com>
2021-04-09 20:04:28 -0300

    change

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function displayTime(time) {
 
 async function getContent() {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=brasilia&appid=25efa7c592ce21bce5ebb66fbe896087`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=brasilia&appid=${apikey}`);
     const result = await response.json();
 
     show(result);
